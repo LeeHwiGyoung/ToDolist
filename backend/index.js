2aef9c4c9e65df7eb0b63700764561d3750558b0
HwiGyoung Lee <ter0000@naver.com>
2024-09-28 15:02:57 +0900

    ✨ post 메서드 todoitem을 인풋으로 받는것을 변경

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import fs from "fs";
+import fs, { write } from "fs";
 import cors from "cors";
 import path from "path";
 
@@ -11,17 +11,84 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-const dummyData = fs.readFileSync("./test.json", "utf-8");
-const jsonDummy = JSON.parse(dummyData);
+function writeTodo(toDoItem) {
+  const json = fs.readFileSync(__dirname + "/toDo.json");
+  const data = JSON.parse(json);
+  const toDoList = data.toDoList;
+  let last_id = toDoList.length !== 0 ? Number(toDoList.slice(-1)[0].id) : -1;
+  const newToDo = toDoItem;
+  newToDo.id = last_id + 1;
+  const saveData = [...data.toDoList, newToDo];
+  return JSON.stringify(saveData);
+}
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/todolist", (req, res) => {
-  const checkUser = jsonDummy.user.find((item) => {
-    return item.userName === req.query.userName;
-  });
-  if (checkUser) {
-    res.json(checkUser);
+app.get("/toDoList", (req, res) => {
+  res.header("Content-Type:application/json");
+  if (!fs.existsSync(__dirname + "/toDo.json")) {
+    fs.writeFileSync(__dirname + "/toDo.json", '{"toDoList" : []}');
+  }
+  res.sendFile(__dirname + "/toDo.json");
+});
+
+app.post("/toDo", (req, res) => {
+  res.header("Content-Type:application/json");
+  if (req.body) {
+    const data = writeTodo(req.body);
+    fs.writeFileSync(
+      __dirname + "/toDo.json",
+      `{ "toDoList" : ${data} }`,
+      (error) => {
+        console.log(error);
+      }
+    );
+    const toDoList = fs.readFileSync(__dirname + "/toDo.json");
+    res.send(toDoList);
+  }
+});
+
+app.delete("/toDo", (req, res) => {
+  console.log(req.query.id);
+  let text = "";
+  const toDoList = JSON.parse(fs.readFileSync(__dirname + "/toDo.json"));
+  const filterdToDoList = JSON.stringify(
+    toDoList.toDoList.filter((item, idx) => {
+      if (item.id !== req.query.id) {
+        return item;
+      } else {
+        text = `{ toDo : ${item.toDo}, isDone : ${item.isDone} } 이 삭제되었습니다.`;
+      }
+    })
+  );
+
+  fs.writeFileSync(
+    __dirname + "/toDo.json",
+    `{"toDoList" : ${filterdToDoList}}`
+  );
+  res.send(text);
+});
+
+app.put("/toDo", (req, res) => {
+  res.header("Content-Type:application/json");
+  if (req.body) {
+    console.log(req.body);
+    /* const data = { todoList: req.body.todoList };
+    for (const [idx, item] of data.todoList.entries()) {
+      item.id = `${idx + 1}`;
+    }
+    fs.writeFileSync(
+      __dirname + "/todo.json",
+      JSON.stringify(data),
+      (error) => {
+        console.log(error);
+      }
+    ); */
+    const toDoList = JSON.parse(fs.readFileSync(__dirname + "/toDo.json"));
+
+    res.json(toDoList);
   }
 });
 
